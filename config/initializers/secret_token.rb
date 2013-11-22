fbdbb7c107312fb7af88ec53b1b033f0655a74db
Kir Shatrov <shatrov@me.com>
2013-11-22 19:27:11 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capblog::Application.config.secret_key_base = 'f05c7ee9dbe234dc506567ac8023eec1568bcdacd2eacfcc04d3d7a89fafc3111c8a2a9db70e45691970a615a6e2ebb6edbeca1c8442849f168a08299c46959c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Capblog::Application.config.session_store :cookie_store, key: '_capblog_session'
